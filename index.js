f55814f0f4a280c67c7746f9876e4df0bcd181e4
Kim Merino <dafabulousteach@gmail.com>
2015-08-27 16:52:58 -0700

    added route to serve dummy profile page

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 "use strict";
 
 var express = require('express');
-
 var app = express();
 var path = require('path');
-
 var port = process.env.PORT || '3000';
 
 //========================================================//
@@ -16,12 +14,43 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+//========================================================//
+//   Serving the client static files                      //
+//========================================================//
 app.use(express.static(path.normalize(__dirname + '/')));
+// app.use(express.static(path.normalize(__dirname + '/profile')));
+//========================================================//
+//   Routes                                               //
+//========================================================//
 
 app.get('/', function(req, res) {
   // res.sendFile(path.join(__dirname + '/index.html'));
   res.send('HELLOOOOOO GAP BAND');
+});
+
+app.get('/profile', function(req, res) {
+  // res.sendFile(path.join(__dirname + '/profile.html'));
+});
+
+//========================================================//
+//   Database Routes                                      //
+//========================================================//
+
+app.post('/profile/habit', function(req, res){
+//   pg.connect(connectionString, function(err, client, done){
+//     var query = client.query('SELECT text, vote FROM topics');
+//     var rows = []; // Array to hold values returned from database
+//     if (err) {
+//       return console.error('error running query', err);
+//     }
+//     query.on('row', function(row) {
+//       rows.push(row);
+//     });
+//     query.on('end', function(result) {
+//       client.end();
+//       return res.json(rows);
+//     });
+//   });
 });
 
 //========================================================//
@@ -31,3 +60,4 @@ var server = app.listen(port, function() {
   var host = server.address().address;
   console.log('The Gap Band app listening at http://%s:%s -- %s', host, port);
 });
+
