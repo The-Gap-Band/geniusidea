171e80b2ff989402a846f7ec52c4ca240c4ba3bc
Kim Merino <dafabulousteach@gmail.com>
2015-08-26 20:08:47 -0700

    test server runs tests run

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -4,4 +4,31 @@ var chai = require('chai');
 var assert = require('assert');
 var expect = require('chai').expect;
 
-var server = require('../index.js'); //server;
+var server = require('../index.js');//.server;
+
+describe('SERVER', function() {
+  describe('Endpoints Exist', function() {
+    
+    it('should respond to GET requests for "/" with a 200 status code', function(done) {
+      request('http://localhost:3000/', function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    // it('should respond to GET requests for "/songs" with a 200 status code', function(done) {
+    //   request('http://127.0.0.1:3000/songs', function(error, response, body) {
+    //     expect(response.statusCode).to.equal(200);
+    //     done();
+    //   });
+    // });
+
+    // xit('should respond to GET requests for "/song" with a 200 status code', function(done) {
+    //   request('http://127.0.0.1:3000/song', function(error, response, body) {
+    //     expect(response.statusCode).to.equal(200);
+    //     done();
+    //   });
+    // });
+    
+  });
+});
