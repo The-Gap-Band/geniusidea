90a629bc89b0c5c994c88357b86b4c185b12178d
Kim Merino <dafabulousteach@gmail.com>
2015-08-26 19:09:57 -0700

    Added spec to hold test files Added dependencies to package file

---
+++ spec/serverSpec.js
@@ -0,0 +1,7 @@
+var request = require('request');
+var mocha = require('mocha');
+var chai = require('chai');
+var assert = require('assert');
+var expect = require('chai').expect;
+
+var server = require('../index.js'); //server;
