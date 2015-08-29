10f3dde21393a7bae2bf2a46cdc4ae96a412a273
TGAStudent <tga.student@telegraphacademy.com>
2015-08-28 20:46:57 -0700

    checkboxes

--- components/profileCheckInCurrentHab.js
+++ components/profileCheckInCurrentHab.js
@@ -2,16 +2,17 @@ var ProfileCheckInCurrentHab = React.createClass({
     getInitialState: function() {
       return {data: this.props.data};
     },
+    
     render: function() {
+        function check(e){e.preventDefault(); console.log('fwdeifwefdhweiuhfwuhef')}
         return (
           <table><tbody>
             <thead><h3>Check in For Today</h3></thead>
-              {this.props.currentHabits.map(function(cell) {
-                return <tr>
-                     <td>{cell}</td>
-                </tr>
-              })}
-                
+              <form action="" method="get">
+                {this.props.currentHabits.map(function(cell) {
+                  return <tr><td><input type="checkbox" name="" value="" onChange={check()}/> {cell}</td></tr>
+                })}
+              </form>
           </tbody></table>
         );
     }
@@ -19,6 +20,9 @@ var ProfileCheckInCurrentHab = React.createClass({
 
 React.render(<ProfileCheckInCurrentHab currentHabits={["Become the best rapper alive","Learn Javascript and stuff","Get super buff by pumping the iron"]} />,document.getElementById("checkincurrenthab"));
 
+              // {this.props.currentHabits.map(function(cell) {
+              //   return <tr><td>{cell}</td></tr>
+              // })}
 
 
 
@@ -28,8 +32,16 @@ React.render(<ProfileCheckInCurrentHab currentHabits={["Become the best rapper a
 
 
 
-
-
+                // <form action="" method="get">
+                //     {this.props.currentHabits.map(function(cell) {
+                
+                      
+                //       return <tr><td><input type="checkbox" name="" value=""> {cell}</td></tr><br>
+                      
+                //     })}
+                  
+                //   <input type="submit" value="Submit">
+                // </form>
 
 
 
