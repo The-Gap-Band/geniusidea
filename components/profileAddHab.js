097ccf7f3cf41d3db44b835ea6a3dfcf2ca002b1
TGAStudent <tga.student@telegraphacademy.com>
2015-08-29 11:32:10 -0700

    able to enter new habits and catch them into an array

--- components/profileAddHab.js
+++ components/profileAddHab.js
@@ -1,20 +1,29 @@
+// var helper = require('./helpers/restHelper.js');
+
 var ProfileAddHab = React.createClass({
+  
   getInitialState: function() {
     return {value: 'Add new Habitudes'};
   },
   handleChange: function(event) {
     this.setState({value: event.target.value});
-    console.log('value: ', this.state.value);
+    // console.log('value: ', this.state.value);
   },
   clear: function(event){
     this.setState({value: ''});
   },
   addItem: function(e){
     e.preventDefault();
+    
+    this.props.habitsArr.push({newHabit: this.state.value})
+    // console.log('this.state.value :',this.state.value);
+    console.log('habitsArr : ',this.props.habitsArr);
+    // helper.post('/api/addHabit',e);
     console.log('Inside addItem function');
-
     // Need to figure out how get value of input
     // and make an AJAX call with input as JSON input
+
+   
   },
   render: function() {
     var value = this.state.value;
@@ -27,4 +36,27 @@ var ProfileAddHab = React.createClass({
   }
 });
 
-React.render(<ProfileAddHab />, document.getElementById("addhab"))
+React.render(<ProfileAddHab habitsArr={[]}/>, document.getElementById("addhab"))
+
+
+
+///////////
+
+//array of JSON objects for habits
+// var data = [
+//   {author: "Pete Hunt", text: "This is one comment"},
+//   {author: "Jordan Walke", text: "This is *another* comment"}
+// ];
+
+// // 
+// var CommentBox = React.createClass({
+//   render: function() {
+//     return (
+//       <div className="commentBox">
+//         <h1>Comments</h1>
+//         <CommentList data={this.props.data} />
+//         <CommentForm />
+//       </div>
+//     );
+//   }
+// });
--- helpers/restHelper.js
+++ helpers/restHelper.js
@@ -1,26 +1,28 @@
 var $ = require('jquery');
 
 module.exports = {
-  get: function(url){
-    return new Promise(function(success, error){
-      $.ajax({
-        url: url,
-        dataType: 'json',
-        success: success,
-        error: error
-      });
-    });
-  },
 
-  post: function(url, data){
-    return new Promise(function(success, error){
-      $.ajax({
-        url: url,
-        dataType: 'json',
-        data: data,
-        success: success,
-        error: error
+    get: function(url){
+      return new Promise(function(success, error){
+        $.ajax({
+          url: url,
+          dataType: 'json',
+          success: success,
+          error: error
+        });
       });
-    });
-  };
-}  
+    },
+
+    post: function(url, data){
+      return new Promise(function(success, error){
+        $.ajax({
+          url: url,
+          type: 'POST',
+          data: data,
+          success: success,
+          error: error
+        });
+      });
+    };
+ 
+}
