f95ea858973992154cec6135d937f4171173584b
TGAStudent <tga.student@telegraphacademy.com>
2015-08-28 21:19:26 -0700

    added check boxes to checkin current habit section, got the onchange event to console.log a message for every check and uncheck

--- components/profileCheckInCurrentHab.js
+++ components/profileCheckInCurrentHab.js
@@ -1,16 +1,14 @@
 var ProfileCheckInCurrentHab = React.createClass({
     getInitialState: function() {
-      return {data: this.props.data};
+      return {data: this.props.data, foo:true};
     },
-    
     render: function() {
-        function check(e){e.preventDefault(); console.log('fwdeifwefdhweiuhfwuhef')}
         return (
           <table><tbody>
             <thead><h3>Check in For Today</h3></thead>
               <form action="" method="get">
                 {this.props.currentHabits.map(function(cell) {
-                  return <tr><td><input type="checkbox" name="" value="" onChange={check()}/> {cell}</td></tr>
+                  return <tr><td><input type="checkbox" name="" value="" onChange={function(){console.log('dsdfvsdf')} }/> {cell}</td></tr>
                 })}
               </form>
           </tbody></table>
@@ -19,34 +17,4 @@ var ProfileCheckInCurrentHab = React.createClass({
 });
 
 React.render(<ProfileCheckInCurrentHab currentHabits={["Become the best rapper alive","Learn Javascript and stuff","Get super buff by pumping the iron"]} />,document.getElementById("checkincurrenthab"));
-
-              // {this.props.currentHabits.map(function(cell) {
-              //   return <tr><td>{cell}</td></tr>
-              // })}
-
-
-
-
-
-
-
-
-
-                // <form action="" method="get">
-                //     {this.props.currentHabits.map(function(cell) {
-                
-                      
-                //       return <tr><td><input type="checkbox" name="" value=""> {cell}</td></tr><br>
-                      
-                //     })}
-                  
-                //   <input type="submit" value="Submit">
-                // </form>
-
-
-
-
-
-
-
 
