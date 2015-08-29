126b1b08a110d0afc62a4d9eea759defbd24fc17
TGAStudent <tga.student@telegraphacademy.com>
2015-08-28 19:42:04 -0700

    fixed order of page elements and reqrote some examples

--- components/profileHabProgress.js
+++ components/profileHabProgress.js
@@ -2,7 +2,7 @@ var ProfileHabProgress = React.createClass({
   render: function() {
     var results = this.props.habitudes;
     return (
-      <div><p>Check in for todays goals</p>
+      <div><h3>Check in for todays goals</h3>
         <ul>
           {results.map(function(result) {
             return <li key={result.id}>{result}</li>;
@@ -13,4 +13,4 @@ var ProfileHabProgress = React.createClass({
   }
 })
 
-React.render(<ProfileHabProgress habitudes={[12,34,56]} />, document.getElementById("habprogress"))
+React.render(<ProfileHabProgress habitudes={['12/30 day streak',"29/30 day streak","7/30 day streak"]} />, document.getElementById("habprogress"))
