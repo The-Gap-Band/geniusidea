4fe42572df067c8451f76cceac0293ac8a5ce19a
TGAStudent <tga.student@telegraphacademy.com>
2015-08-28 19:10:13 -0700

    fixed table of current habits

--- components/profileAddHab.js
+++ components/profileAddHab.js
@@ -1,16 +1,59 @@
 var ProfileAddHab = React.createClass({
+  getInitialState: function() {
+    return {value: 'Add new Habitudes'};
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+    console.log('value :',value)
+  },
+  clear: function(event){
+    this.setState({value: null});
+  },
+  foo: function(e){
+    e.preventDefault();
+    console.log('HELLOOOOOOOOOOOOOOOOO')
+  },
   render: function() {
-    var results = this.props.list;
+
+    var value = this.state.value;
     return (
-      <div><p>Check in for todays goals</p>
-        <ol>
-          {results.map(function(result) {
-            return <li key={result.id}>{result}</li>;
-          })}
-        </ol>
+      <div>
+        <form onSubmit={this.foo}><input type="text" value={value} onChange={this.handleChange} onClick={this.clear} id="user_input"/>
+        <button type="button">Enter Habitude</button></form>
+
+        
+
       </div>
     );
   }
+    
+  
 });
 
-React.render(<ProfileAddHab list={["xyz","xyz","xyz"]}/>, document.getElementById("addhab"))
+React.render(<ProfileAddHab />, document.getElementById("addhab"))
+
+
+//     function howMany(){
+//       var message_entered =  document.getElementById("user_input").value
+//       document.getElementById('display').innerHTML = message_entered;
+//     }
+// <input type="submit" onclick="howMany();">
+//         <p><span id='display'></span> </p>
+// function delcaration
+// <script>
+//   function howMany(){
+//     var message_entered =  document.getElementById("user_input").value
+//     document.getElementById('display').innerHTML = message_entered;
+//   }
+// </script>
+
+// where to enter number
+// <form>
+//   <label><b>Enter how many minutes</b></label>
+//   <input type="text" name="message" id="user_input">
+// </form>
+
+// where to display number
+//   <input type="submit" onclick="howMany();"><br />
+//   <label>To how many Ignitions</label>
+//   <p><span id='display'></span> </p>
--- components/profileCurrentHab.js
+++ components/profileCurrentHab.js
@@ -1,23 +1,23 @@
-var ProfileCurrentHab = React.createClass({
+// var ProfileCurrentHab = React.createClass({
 
-  render: function() {
-    var results = this.props.list;
-    return (
-      <div>
-        <thead>Check in for todays goals</thead>
-        <tr>
-          {results.map(function(result) {
-            console.log('result',result)
-            return <td key={result.id}> {result} </td>;
-          })}
-        </tr>
-      </div>
+//   render: function() {
+//     var results = this.props.list;
+//     return (
+//       <div>
+//         <thead>Check in for todays goals</thead>
+//         <tr>
+//           {results.map(function(result) {
+//             console.log('result',result)
+//             return <td key={result.id}> {result} </td>;
+//           })}
+//         </tr>
+//       </div>
       
-    );
-  }
-});
+//     );
+//   }
+// });
 
-React.render(<ProfileCurrentHab list={["Become the best rapper alive","Learn Javascript and stuff","Beat Vy in arm wrestling"]} /> , document.getElementById("currenthab"))
+// React.render(<ProfileCurrentHab list={["Become the best rapper alive","Learn Javascript and stuff","Beat Vy in arm wrestling"]} /> , document.getElementById("currenthab"))
 
 
 
@@ -37,3 +37,49 @@ React.render(<ProfileCurrentHab list={["Become the best rapper alive","Learn Jav
 
 
         // 
+
+
+
+
+var CustomRow = React.createClass({
+    getInitialState: function() {
+      return {data: this.props.data};
+    },
+    render: function() {
+        return (
+          <table><tbody>
+            
+              {this.props.currentHabits.map(function(cell) {
+                return <tr>
+                     <td>{cell}</td>
+                </tr>
+              })}
+                
+          </tbody></table>
+        );
+    }
+});
+
+
+
+React.render(<CustomRow currentHabits={["Become the best rapper alive","Learn Javascript and stuff","Beat Vy in arm wrestling"]}/>,document.getElementById("currenthab"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
