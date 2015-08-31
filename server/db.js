5f1f96382d0f2300fdbafe1a846cd04bd4fe22dd
Kim Merino <dafabulousteach@gmail.com>
2015-08-31 11:36:39 -0700

    server at localhost:3000 works

--- server/db.js
+++ server/db.js
@@ -1,17 +1,33 @@
+var express = require('express');
+var app = express();
 var pg = require('pg');
-
+module.exports = function(app){
 //========================================================//
 //   Database Routes                                      //
 //========================================================//
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres';
+var connectionString = 'postgres://localhost:5432/postgres';
 
 app.get('/api/profile', function(req, res){
-  
+   pg.connect(connectionString, function(err, client, done){
+    var query = client.query('SELECT * from users');
+    var rows = []; // Array to hold values returned from database
+    if (err) {
+      return console.error('error running query', err);
+    }
+    query.on('row', function(row) {
+      rows.push(row);
+    });
+    query.on('end', function(result) {
+      client.end();
+      // return res.json(rows);
+      return 'cats';
+    });
+  });
 });
 
 app.post('/api/addHabit', function(req, res){
   pg.connect(connectionString, function(err, client, done){
-    // var query = client.query('SELECT text, vote FROM topics');
+    var query = client.query('INSERT INTO habits (user_id, habit) VALUES (1, ‘biking every day’);');
     // var rows = []; // Array to hold values returned from database
     if (err) {
       return console.error('error running query', err);
@@ -26,9 +42,9 @@ app.post('/api/addHabit', function(req, res){
   });
 });
 
-app.post('/api/updateHabit', function(req, res){
-  pg.connect(connectionString, function(err, client, done){
+// app.post('/api/updateHabit', function(req, res){
+//   pg.connect(connectionString, function(err, client, done){
 
-});
-
-module.exports = db;
+// });
+}
+// module.exports = db;
