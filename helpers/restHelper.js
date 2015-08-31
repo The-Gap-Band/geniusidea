3a859df22bfae3ef57548a86f1ecea6d1862f013
Kim Merino <dafabulousteach@gmail.com>
2015-08-31 11:40:23 -0700

    commented AJAX code and added db connection to app js file

--- helpers/restHelper.js
+++ helpers/restHelper.js
@@ -1,29 +1,32 @@
 var $ = require('jquery');
 
-module.exports = {
-  get: function(url){
-    return new Promise(function(success, error){
-      $.ajax({
-        url: url,
-        type: 'GET',
-        contentType: 'application/json',
-        success: success,
-        error: error
-      });
-    });
-  },
+// module.exports = {
+//   get: function(url){
+//     return new Promise(function(success, error){
+//       $.ajax({
+//         url: url,
+//         type: 'GET',
+//         dataType: 'json'
+//         success: function(data){
+//           console.log('Success');},
+//         error: function(){
+//           console.log('Error, data not found');
+//         };
+//       });
+//     });
+//   },
 
-  post: function(url, data){
-    return new Promise(function(success, error){
-      $.ajax({
-        url: url,
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'application/json'
-        data: data,
-        success: success,
-        error: error
-      });
-    });
-  };
-}  
+  // post: function(url, data){
+  //   return new Promise(function(success, error){
+  //     $.ajax({
+  //       url: url,
+  //       type: 'POST',
+  //       dataType: 'json',
+  //       contentType: 'application/json'
+  //       data: data,
+  //       success: success,
+  //       error: error
+  //     });
+  //   });
+  // };
+// }  
