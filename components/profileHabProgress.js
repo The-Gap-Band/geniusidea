12c45516a290218326fa9dfdeee2753497d70f33
veeweeherman <vy.hoang.cu@gmail.com>
2015-08-31 21:51:54 -0700

    used forloop and predicate to capture results from db query if its type was correct in order to display users' habits in Habits-in-Progress section

--- components/profileHabProgress.js
+++ components/profileHabProgress.js
@@ -18,7 +18,7 @@
 var ProfileHabProgress = React.createClass({
   getInitialState: function() {
     return {
-      hab: 'Dasdsa'
+      hab: 'Dadadadadadada'
     };
   },
 
@@ -36,23 +36,48 @@ var ProfileHabProgress = React.createClass({
   },
 
   render: function() {
-    console.log('this.state.hab',this.state.hab)
+    // console.log('this.state.hab',this.state.hab)
     // var results = this.state.hab;
     // console.log('thing2 :',this.habitudes)
+    var habList = this.state.hab;
+    console.log('habList',habList)
     
-      
+    for (var i = 0; i < habList.length; i++) {
+      if (typeof habList[i] !== 'string'){
+
+        this.props.example.push(habList[i])
+      }  
+    };  
+    console.log('this.props.example',this.props.example)
+
+
     return (
    
    <div>
       <h3>Habits in Progress</h3>
-        <ul> {this.state.hab}</ul>
+        <ul>{this.props.example.map(function(obj) {
+          console.log('obj-------',obj.habit)
+          return <li key={obj.id}>{obj.habit}</li>;
+        })}</ul>
     </div>
        
     );
   }
 })
-          // {this.state.hab.map(function(result) {
-            // return <li key={result.id}>{result}</li>;
-          // })}
+        // <ul>{habList.map(function(obj) {
+        //     return <li key={obj.id}>{obj.habit}</li>;
+        //   })}</ul>
 
-React.render(<ProfileHabProgress habitudes={'http://localhost:3000/api/habits'} />, document.getElementById("habprogress"))
+
+
+
+
+
+React.render(<ProfileHabProgress example={[]} habitudes={'http://localhost:3000/api/habits'} />, document.getElementById("habprogress"))
+
+
+
+
+
+
+
