a8569c0d9f03d64c301bd74775aa1daaf9e83616
veeweeherman <vy.hoang.cu@gmail.com>
2015-09-01 16:20:29 -0700

    posting to db new habits

--- components/testAddDisplayHab.js
+++ components/testAddDisplayHab.js
@@ -28,6 +28,24 @@ var ProfileAddDisplayHab = React.createClass({ //habit BOX
      }.bind(this)
     });
   },
+  handleHabitSubmit: function(habit) {
+   var habits = this.state.data;
+   var newHabits = habits.concat([habit]);
+   this.setState({data: newHabits});
+   $.ajax({
+     url: this.props.url,
+     dataType: 'json',
+     type: 'POST',
+     data: habit,
+     success: function(data) {
+       this.setState({data: data});
+     }.bind(this),
+     error: function(xhr, status, err) {
+       console.error(this.props.url, status, err.toString());
+     }.bind(this)
+   });
+ },
+
   getInitialState: function() {
     return {data: []};
   },
@@ -40,8 +58,9 @@ var ProfileAddDisplayHab = React.createClass({ //habit BOX
     console.log('this.state.data ',this.state.data)
     return (
       <div className="habitBox">
-       <h1>habitssssssss</h1>
-       <HabitList data={this.state.data} />
+         <h1>habitssssssss</h1>
+         <HabitList data={this.state.data} />
+         <HabitForm onHabitSubmit={this.handleHabitSubmit} />
      </div>
     );
   }
@@ -70,28 +89,28 @@ var HabitList = React.createClass({
  }
 });
 
-// var CommentForm = React.createClass({
-//  handleSubmit: function(e) {
-//    e.preventDefault();
-//    var author = React.findDOMNode(this.refs.author).value.trim();
-//    var text = React.findDOMNode(this.refs.text).value.trim();
-//    if (!text || !author) {
-//      return;
-//    }
-//    this.props.onHabitSubmit({author: author, text: text});
-//    React.findDOMNode(this.refs.author).value = '';
-//    React.findDOMNode(this.refs.text).value = '';
-//  },
-//  render: function() {
-//    return (
-//      <form className="commentForm" onSubmit={this.handleSubmit}>
-//        <input type="text" placeholder="Your name" ref="author" />
-//        <input type="text" placeholder="Say something..." ref="text" />
-//        <input type="submit" value="Post" />
-//      </form>
-//    );
-//  }
-// });
+var HabitForm = React.createClass({
+ handleSubmit: function(e) {
+   e.preventDefault();
+   
+   var habit = React.findDOMNode(this.refs.habit).value.trim();
+   if (!habit) {
+     return;
+   }
+   this.props.onHabitSubmit({habit: habit});
+   
+   React.findDOMNode(this.refs.habit).value = '';
+ },
+ render: function() {
+   return (
+     <form className="habitForm" onSubmit={this.handleSubmit}>
+       
+       <input type="text" placeholder="Say something...creepy..." ref="habit" />
+       <input type="submit" value="Post" />
+     </form>
+   );
+ }
+});
 
 
 React.render(<ProfileAddDisplayHab url={'http://localhost:3000/api/habits'} pollInterval={2000} habitsObj={{}}/>, document.getElementById("addhab"))
