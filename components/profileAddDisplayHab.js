5ccc7d8afc6f4428a03ec77b3c9c0e78961c7f87
veeweeherman <vy.hoang.cu@gmail.com>
2015-09-01 16:51:23 -0700

    cleaned code for adding stuff to db via post request in text field

--- components/profileAddDisplayHab.js
+++ components/profileAddDisplayHab.js
@@ -1,90 +1,104 @@
-// var helper = require('./helpers/restHelper.js');
-
-var ProfileAddDisplayHab = React.createClass({
-  getInitialState: function() {
-    return {value: ''};
-  },
-
-  // componentDidMount: function() {
-  //   $.post(this.props.link, function(result) {
-    
-  //     this.setState({value : result}); // setting the current state to the result of the db-query
-  //   }.bind(this));
-  // },
-  
-  // handleChange: function(event) {
-
-  //   this.setState({value: event.target.value});
-  //   // console.log('event: ', event);
-  // },
-  // clear: function(event){
-  //   this.setState({value: ''});
-  // },
-  addItem: function(e){ //what happens when press enter after inputting text into text field
-    e.preventDefault();
-    // this.props.habitsObj.habit = this.state.value;
-    
-
-    // $.post(this.props.link, function(result) {
-    //   this.setState({value : result}); // setting the current state to the result of the db-query
-    // console.log('Inside addItem function');
-    // }.bind(this));
-
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      type: 'POST',
-      data: {habit: this.refs.habitValue},
-      success: function(data) {
-        this.setState({data: data});  
-        console.log('AIRHORN!!!!!!')
-        
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log('NOOooooooooooo')
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-
-
-
-    // Need to figure out how get value of input
-    // and make an AJAX call with input as JSON input
-  },
+var Habit = React.createClass({
   render: function() {
-
+  console.log('this ',this)
+  var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
     return (
-      <div>
-        <h1>Add Habits Here</h1>
-        <form onSubmit={this.addItem}>
-          <input type="text" placeholder="enter habit here" ref="habitValue" />
-          <button>Enter Habitude</button></form>
+      <div className="habit">
+        <h2 className="user_id">{this.props.user_id}</h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
 });
 
-React.render(<ProfileAddDisplayHab foo={[]} url={'http://localhost:3000/api/habits'} habitsObj={{}}/>, document.getElementById("addhab"))
+var ProfileAddDisplayHab = React.createClass({ //habit BOX
+  loadHabitsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+        console.log('data',data);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleHabitSubmit: function(habit) {
+    var habits = this.state.data;
+    var newHabits = habits.concat([habit]);
+    this.setState({data: newHabits});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: habit,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadHabitsFromServer();
+    setInterval(this.loadHabitsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    console.log('this.state.data ',this.state.data)
+    return (
+      <div className="habitBox">
+        <h1>Current Habitssss</h1>
+        <HabitList data={this.state.data} />
+        <HabitForm onHabitSubmit={this.handleHabitSubmit} />
+      </div>
+    );
+  }
+});
 
+var HabitList = React.createClass({
+  render: function() {
+  console.log('this.props from HabitList func',this.props)
+  var habitNodes = this.props.data.map(function(habit, index) {
+    return (
+    // `key` is a React-specific concept and is not mandatory for the
+    // purpose of this tutorial. if you're curious, see more here:
+    // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+      <Habit user_id={habit.user_id} key={index}>
+        {habit.habit}
+      </Habit>
+    );
+  });
+    return (
+      <div className="HabitList">{habitNodes}</div>
+    );
+  }
+});
 
+var HabitForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var habit = React.findDOMNode(this.refs.habit).value.trim();
+    if (!habit) {
+      return;
+    }
+    this.props.onHabitSubmit({habit: habit});
+    React.findDOMNode(this.refs.habit).value = '';
+  },
+  render: function() {
+    return (
+    <form className="habitForm" onSubmit={this.handleSubmit}>
+      <input type="text" placeholder="Say something...creepy..." ref="habit" />
+      <input type="submit" value="Post" />
+    </form>
+    );
+  }
+});
 
-///////////
-
-//array of JSON objects for habits
-// var data = [
-//   {author: "Pete Hunt", text: "This is one comment"},
-//   {author: "Jordan Walke", text: "This is *another* comment"}
-// ];
-
-// // 
-// var CommentBox = React.createClass({
-//   render: function() {
-//     return (
-//       <div className="commentBox">
-//         <h1>Comments</h1>
-//         <CommentList data={this.props.data} />
-//         <CommentForm />
-//       </div>
-//     );
-//   }
-// });
+React.render(<ProfileAddDisplayHab url={'http://localhost:3000/api/habits'} pollInterval={2000} habitsObj={{}}/>, document.getElementById("adddisplayhab"));
