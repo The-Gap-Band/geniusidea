1e8f99adee3bd667e6f88c44d8acda4bef44d1fb
Kim Merino <dafabulousteach@gmail.com>
2015-09-01 18:55:21 -0700

    adds test to user ability to add habit

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -8,33 +8,29 @@ var server = require('../app.js'); //.server;
 
 describe('SERVER', function() {
   describe('User Functions', function() {
-    // GET THE USER PROFILE
-      // if user profile exists, 
-        // return 200
-      // else 
-        // return 404
 
-    it('should respond ', function(done) {
+    it('should respond to a GET request for a user profile with 200 status code', function(done) {
       request('http://localhost:3000/', function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
       });
     });
+
     // USER CAN ADD A HABIT ON SUBMIT
-    xit('should respond to GET requests for "/profile" with a 200 status code', function(done) {
-      request('http://localhost:3000', function(error, response, body) {
+    it('should route a habit to the database', function(done) {
+      request('http://localhost:3000/api/addHabit', function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
       });
     });
-    // USER CAN UPDATE THEIR HABIT 
-    xit('should send back parsable stringified JSON', function(done) {
-      request('', function(error, response, body) {
-        var req = 
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
+    // // USER CAN UPDATE THEIR HABIT 
+    // xit('should send back parsable stringified JSON', function(done) {
+    //   request('', function(error, response, body) {
+    //     var req = 
+    //     expect(response.statusCode).to.equal(200);
+    //     done();
+    //   });
+    // });
     
   });
 
