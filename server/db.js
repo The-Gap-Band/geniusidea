4eb6a552b7264f94050d15bba8ebbda94ff06300
veeweeherman <vy.hoang.cu@gmail.com>
2015-09-03 20:07:00 -0700

    fixing error of syntax

--- server/db.js
+++ server/db.js
@@ -9,8 +9,6 @@ module.exports = function(app){
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:false}));
 
-<<<<<<< HEAD
-
 
   //========================================================//
   //   Establish Database Connection                        //
