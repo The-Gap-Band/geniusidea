d0476617fd3366e5cfc070c56a01ecf5e250c1af
Kim Merino <dafabulousteach@gmail.com>
2015-09-04 16:49:24 -0700

    added supertest to package file and GET test

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -1,28 +1,31 @@
-var request = require('request');
+var request = require('supertest');
 var mocha = require('mocha');
-var chai = require('chai');
 var assert = require('assert');
+var express = require('express');
 var expect = require('chai').expect;
+var server = require('../app.js'); //the server;
+request = request('localhost:3000');
 
-var server = require('../app.js'); //.server;
-
-describe('SERVER', function() {
-  describe('User Functions', function() {
-
-    it('should respond to a GET request for a user profile with 200 status code', function(done) {
-      request('http://localhost:3000/', function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
+describe('API', function() {
+  describe('handle http requests', function() {
+    it('should respond to a GET request for a user profile with 200 status code', function(done){
+      request.get('/').expect(200, done);
     });
+    // it('should respond to a GET request for a user profile with 200 status code', function(done) {
+    //   request('http://localhost:3000/', function(error, response, body) {
+    //     expect(response.statusCode).to.equal(200);
+    //     done();
+    //   });
+    // });
 
     // USER CAN ADD A HABIT ON SUBMIT
-    tit('should route a habit to the database', function(done) {
-      request('http://localhost:3000/api/addHabit', function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
+    // it('should route a habit to the database', function(done) {
+    //   request('http://localhost:3000/api/addHabit', function(error, response, body) {
+    //     expect(response.statusCode).to.equal(200);
+    //     done();
+    //   });
+    // });
+
     // // USER CAN UPDATE THEIR HABIT 
     // xit('should send back parsable stringified JSON', function(done) {
     //   request('', function(error, response, body) {
