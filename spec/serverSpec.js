479a4e235e56d085edf6abc348897105d3a1133d
Kim Merino <dafabulousteach@gmail.com>
2015-09-04 12:18:56 -0700

    edited get req for home page

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -7,26 +7,16 @@ var expect = require('chai').expect;
 var server = require('../app.js'); //.server;
 
 describe('SERVER', function() {
-  describe('User Functions', function() {
-    // GET THE USER PROFILE
-      // if user profile exists, 
-        // return 200
-      // else 
-        // return 404
+  describe('Request Methods', function() {
 
-    it('should respond ', function(done) {
+    it('should serve the home page ', function(done) {
       request('http://localhost:3000/', function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
       });
     });
-    // USER CAN ADD A HABIT ON SUBMIT
-    xit('should respond to GET requests for "/profile" with a 200 status code', function(done) {
-      request('http://localhost:3000', function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
+    // SERVER RESPONDS TO ADD A HABIT ON SUBMIT
+    
     // USER CAN UPDATE THEIR HABIT 
     xit('should send back parsable stringified JSON', function(done) {
       request('', function(error, response, body) {
