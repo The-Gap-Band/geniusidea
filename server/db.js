45c121bef4f1bd81ba96097ec2e9c26893810d53
jdlawrence <jamil.lawrence@gmail.com>
2015-09-04 11:11:30 -0700

    Commits to prepare for pull from upstream master

--- server/db.js
+++ server/db.js
@@ -150,8 +150,9 @@ module.exports = function(app){
       // Posts an update to the 'updates' table where the habit_id matches that of the input habit string
       // CURL COMMAND: curl -X POST -d "habit='biking'" localhost:3000/api/updateHabit
       // will update the 'biking' habit
-      var getIDQuery = "(SELECT DISTINCT habits.habit_id FROM habits " + 
-                      "WHERE habits.habit = '" + habit + "')";
+
+     var getIDQuery = "(SELECT DISTINCT habits.habit_id FROM habits " + 
+                       "WHERE habits.habit = '" + habit + "')";
 
       var query = client.query("INSERT INTO updates (habit_id) " +
                                "VALUES (" + getIDQuery + ")");
