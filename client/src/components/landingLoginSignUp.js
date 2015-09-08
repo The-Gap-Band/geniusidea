0b142bbab2e7dd097e63d95392be4f1860622290
TGAStudent <tga.student@telegraphacademy.com>
2015-09-08 14:58:21 -0700

    adding to landing page

--- client/src/components/landingLoginSignUp.js
+++ client/src/components/landingLoginSignUp.js
@@ -2,11 +2,24 @@ var LoginSignUp = React.createClass({
 	login: function(){
 		console.log('existing user LOG IN!!!!!!');
 	},
-	signUp: function(){
+	signUp: function(usernamePassword){
 		// checks db for exisiting username
 		// if username doesnt already exist, inserts new username into users table, and logs new user into their new account
 		// if username already exists, error message "user w that username already exists"
 		console.log('new user SIGN UP!!!!!!');
+
+		$.ajax({
+      url: '/api/habits',
+      dataType: 'json',
+      type: 'POST',
+      data: usernamePassword,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
 	},
 	render: function(){
 		return (
@@ -20,4 +33,4 @@ var LoginSignUp = React.createClass({
 	  )
 	}
 });
-React.render(<LoginSignUp />, document.getElementById('logninsignup'));
+React.render(<LoginSignUp url={'/api/signup'}/>, document.getElementById('logninsignup'));
--- server/db.js
+++ server/db.js
@@ -14,7 +14,7 @@ module.exports = function(app){
 //========================================================//
 /*Change the database name to your local machine's name*/
   var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/veeweeherman';
-
+// export DATABASE_URL='postgres://mlsnfeluxqiuff:9ChVkwF-1ypBrOsmB_kNV8rEDi@ec2-54-197-245-93.compute-1.amazonaws.com:5432/de5lornqrnncva'
   // var connectionString = process.env.DATABASE_URL || 'postgres://mlsnfeluxqiuff:9ChVkwF-1ypBrOsmB_kNV8rEDi@ec2-54-197-245-93.compute-1.amazonaws.com:5432/de5lornqrnncva';
 
   //========================================================//
