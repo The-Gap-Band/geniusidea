80fea22b83c0e33f85988cae447d53d325491aab
jdlawrence <jamil.lawrence@gmail.com>
2015-09-08 11:40:32 -0700

    Fixing all merge conflicts

--- server/db.js
+++ server/db.js
@@ -13,20 +13,11 @@ module.exports = function(app){
 //   Establish Database Connection                        //
 //========================================================//
 /*Change the database name to your local machine's name*/
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   // var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/kmerino';
 
   var connectionString = process.env.DATABASE_URL || 'postgres://mlsnfeluxqiuff:9ChVkwF-1ypBrOsmB_kNV8rEDi@ec2-54-197-245-93.compute-1.amazonaws.com:5432/de5lornqrnncva';
-=======
-  // var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/veeweeherman';
-=======
-  var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres';
->>>>>>> Fixing commit errors
 
-  // var connectionString = process.env.DATABASE_URL || 'postgres://mlsnfeluxqiuff:9ChVkwF-1ypBrOsmB_kNV8rEDi@ec2-54-197-245-93.compute-1.amazonaws.com:5432/de5lornqrnncva';
-
->>>>>>> Updated merge conflicts
 
   //========================================================//
   //   Database Queries                                     //
