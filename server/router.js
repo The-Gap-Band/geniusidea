1c6a45ab323538afcb3f0a13be068c24eb2b2c62
Kim Merino <dafabulousteach@gmail.com>
2015-09-09 11:23:43 -0700

    removed api call to server landing.html, renames landing -> index and index -> profile

--- server/router.js
+++ server/router.js
@@ -2,15 +2,10 @@ var express = require('express');
 var app = express();
 var db = require('./db.js');
 module.exports = function(app){
-
-  app.get('/landing', function(req, res) {
-    res.sendFile(path.join(__dirname + '/client/landing.html'));
-});
-
   app.post('/api/signup', function (req, res){
     db.newUser(req, res);
     if(!err){
-      res.redirect(302, '/profile/');
+      res.redirect(302, '/profile.html');
     } else {
       res.redirect(302, '/signup');
     }
@@ -23,7 +18,7 @@ module.exports = function(app){
     } else {
       user.comparePassword(req, res);
       if(!err){
-        res.redirect(302, '/profile/');
+        res.redirect(302, '/profile');
       } else {
         res.redirect('/login');
       }
