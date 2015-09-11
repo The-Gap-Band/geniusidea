79f40662e95e5de1bc0bfbc1b238961bc0b6a0bd
veeweeherman <vy.hoang.cu@gmail.com>
2015-09-11 12:04:20 -0700

    refactored name/loc component to combine with pic component as one

--- client/src/components/profilePicture.js
+++ client/src/components/profilePicture.js
@@ -1,6 +1,36 @@
 var ProfilePicture = React.createClass({
+	  getInitialState: function(){ // MAKE SURE TO SET INITIAL STATE TO INCLUDE THE PARAMS OF THE ITEMS YOU NEED FROM THE DB-RETURN-OBJECT DUH
+    return {username: '',location: ''}
+  }, 
+  
+  componentDidMount: function(x,y,z) {
+
+    var foo;
+    $.ajax({
+        type: 'GET',
+        url: '/api/nameAndLoc', /* NEEDS THE GET REQUEST FOR USERNAME AND LOCATION */
+        dataType: 'json',
+        success: function(data) { // After the query has return the object, set the state's parameters to equal the values needed from the db-query-object
+          this.setState({
+            username: data[0].username, 
+            location: data[0].location
+          })
+          console.log('successfully retrieved username and location from the database!')
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+          console.log('failed to retrieve username and location from db :( ');
+        }.bind(this)
+    })
+  },
 	render: function(){
-		return (<img className="profilepic" src={'http://40.media.tumblr.com/tumblr_makm5yaqVk1r8b83ro1_500.jpg'} />) // pulls the image for user's profile
+		return (
+			<div>
+        <img className="profilepic" src={'http://40.media.tumblr.com/tumblr_makm5yaqVk1r8b83ro1_500.jpg'} />
+        <p>NAME: {this.state.username}</p>
+        <p>LOCATION: {this.state.location}</p>
+      </div>
+    ) // pulls the image for user's profile
 	}
 })
 // TODO: photo needs to be queried from db of users'profile photos
